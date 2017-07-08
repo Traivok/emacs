bc0db816a2526202bdd58e8986841ee3c9c5bee7
Traivok <jraf@cin.ufpe.br>
2017-07-08 11:16:23 -0300

    First

---
+++ init.el
@@ -0,0 +1,82 @@
+;; load emacs 24's package system. Add MELPA repository.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
+
+;; disable bars
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1) 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (dracula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;          Load packages and theirs configurations          ;;
+(defun load-directory (dir)
+  (let ((load-it (lambda (f)
+		   (load-file (concat (file-name-as-directory dir) f)))
+		 ))
+    (mapc load-it (directory-files dir nil "\\.el$"))))
+(load-directory "~/.emacs.d/Settings/")
+;; End
+
+;;            Set backup directory
+(setq backup-directory-alist `(("." . "/home/ricardo/Development/.backup")))
+(setq delete-old-versions t
+  kept-new-versions 2
+  kept-old-versions 2
+  version-control t)
+(put 'scroll-left 'disabled nil)
+;; End
+
+;; Add highlight matching parenthesis
+(show-paren-mode 1)
+
+;; open.h files as c++-mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; set Terminus font 
+(set-default-font "Terminus 12")
+
+;; Display, Troggle, enable...
+(define-global-minor-mode my-linum-mode linum-mode
+  (lambda ()
+    (when (not (memq major-mode
+                     (list 'term-mode 'shell-mode 'eshell-mode)))
+      (linum-mode t))))
+(my-linum-mode 1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq linum-format "%d ")
+(toggle-scroll-bar -1)
+(fringe-mode -1)
+;; End
+
+;;             Display columns
+(setq column-number-mode t)
+
+;; no startup msg  
+(setq inhibit-startup-message t) ; Disable startup message
+
+;; load theme
+(load-theme 'dracula t)
