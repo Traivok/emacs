3760d1abb048392bd4be8037dbca6aaa79f25d79
Traivok <jraf@cin.ufpe.br>
2017-07-23 12:50:42 -0300

    Package manager

---
+++ Settings/Misc.el
@@ -0,0 +1,11 @@
+;;; Misc.el --- miscellanea
+;;; Commentary:
+;;; Enviroment configuration
+
+;;; Code:
+
+
+
+
+(provide 'Misc)
+;;; Misc.el ends here
--- Settings/my-installed-packages.el
+++ Settings/my-installed-packages.el
@@ -4,20 +4,22 @@
 
 ;;; Code:
 
-(defvar my-package-list '(auto-highlight-symbol cmake-ide cmake-mode company-c-headers irony company-irony company-irony-c-headers
-						rtags company-rtags flycheck-irony flycheck-rtags magit monokai-theme smart-comment yasnippet)
-  "All my packages list.")
+(defvar my-package-list nil "All my packages list.")
+
+(setq my-package-list '(auto-highlight-symbol cmake-ide cmake-mode company-c-headers irony company-irony company-irony-c-headers
+						rtags company-rtags flycheck-irony flycheck-rtags magit monokai-theme smart-comment yasnippet))
+
+;; update database
+(unless package-archive-contents (package-refresh-contents))
 
 ;; check if any package of my-package-list isn't installed and install it
 (dolist (my-package my-package-list)
-  (when (require 'my-package nil 'noerror)
-      (package-install 'my-package)))
-;; end
+  (unless (package-installed-p my-package)
+    (package-install my-package)))
 
 ;; update packages
 (when (not package-archive-contents)
     (package-refresh-contents))
-;; end
 
 (provide 'my-installed-packages)
 ;;; my-installed-packages.el ends here
