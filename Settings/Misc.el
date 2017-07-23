5afaf07b75245df1e308924b3aaeb1d91235dff4
Traivok <jraf@cin.ufpe.br>
2017-07-23 13:29:53 -0300

    Cleaning buffers

--- Settings/Misc.el
+++ Settings/Misc.el
@@ -4,7 +4,10 @@
 
 ;;; Code:
 
+(require 'midnight)
+(setq midnight-period (* 2 60 60))
 
+(add-to-list 'clean-buffer-list-kill-regexps '("magit"))
 
 
 (provide 'Misc)
--- Settings/my-installed-packages.el
+++ Settings/my-installed-packages.el
@@ -7,7 +7,8 @@
 (defvar my-package-list nil "All my packages list.")
 
 (setq my-package-list '(auto-highlight-symbol cmake-ide cmake-mode company-c-headers irony company-irony company-irony-c-headers
-						rtags company-rtags flycheck-irony flycheck-rtags magit monokai-theme smart-comment yasnippet))
+					      rtags company-rtags flycheck-irony flycheck-rtags magit monokai-theme smart-comment yasnippet
+					      midnight))
 
 ;; update database
 (unless package-archive-contents (package-refresh-contents))
