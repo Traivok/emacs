c8875b35f126cb3195639330c8970c22eccea217
Traivok <jraf@cin.ufpe.br>
2017-07-28 17:46:48 -0300

    Update

--- Settings/Misc.el
+++ Settings/Misc.el
@@ -38,27 +38,5 @@
   (comint-send-input nil t))
 ;; end
   
-;; kill unused buffer
-(require 'tempbuf "~/.emacs.d/Settings/tempbuf.el")
-(add-hook 'custom-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'w3-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'Man-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'view-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'after-change-major-mode-hook 'turn-on-tempbuf-mode)
-(add-hook 'find-file-hooks 'turn-on-tempbuf-mode)
-
-(defun my-term-on-tempbuf-expire ()
-  "Killing idle terminal."
-  (when (get-buffer-process (current-buffer))
-    (comint-send-eof)))
-
-(defun my-term-mode-patch ()
-  "Turning tempbuf on."
-  (turn-on-tempbuf-mode)
-  (add-hook 'tempbuf-expire-hook 'my-term-on-tempbuf-expire
-	    nil t))
-(add-hook 'term-mode-hook 'my-term-mode-patch)
-;; end
-
 (provide 'Misc)
 ;;; Misc.el ends here
