2131d410e646307f5aa44c4168fd18d030df451b
Traivok <jraf@cin.ufpe.br>
2018-01-18 18:39:32 -0300

    Web dev mode kbds

--- setup.el
+++ setup.el
@@ -131,6 +131,9 @@
 (require 'rtags)
 (require 'company-rtags)
 (require 'yasnippet)
+(require 'js2-mode)
+(require 'js2-refactor)
+(require 'xref-js2)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
@@ -192,13 +195,21 @@ Otherwise, just insert the typed character."
 (add-hook 'c++-mode-hook (lambda () (defvar flycheck-gcc-language-standard "c++14" "Set GCC standart to C++14")))
 (add-hook 'c++-mode-hook (lambda () (defvar flycheck-clang-language-standard "c++14" "Set Clang standart to C++14")))
 
-(require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
 
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
 (require 'magit)
 
